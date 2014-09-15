8ee4d06a2481e16bc017424d3ca5502ea5de9e62
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-15 10:10:20 +0000

    Initial Version Release
    git-svn-id: https://plugins.svn.wordpress.org/wp-lightbox-bank/trunk@990025 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/wp-lightbox-bank-settings-class.php
@@ -0,0 +1,78 @@
+<?php
+switch($wp_role)
+{
+	case "administrator":
+		$user_role_permission = "manage_options";
+	break;
+	case "editor":
+		$user_role_permission = "publish_pages";
+	break;
+	case "author":
+		$user_role_permission = "publish_posts";
+	break;
+}
+
+if (!current_user_can($user_role_permission))
+{
+	return;
+}
+else
+{ 
+	if(isset($_REQUEST["param"]))
+	{
+		if($_REQUEST["param"] == "update_lightbox_settings")
+		{
+			$lightbox_setting_array = array();
+			$lightbox_setting_array["wp_galleries"] = isset($_REQUEST["ux_chk_galleries"]) ? "1" :"0" ;
+			$lightbox_setting_array["wp_caption_image"] = isset($_REQUEST["ux_chk_imagecaption"]) ? "1" : "0";
+			$lightbox_setting_array["attachment_image"]= isset($_REQUEST["ux_chk_attachmentimage"]) ? "1" : "0";
+			$lightbox_setting_array["overlay_click"]= isset($_REQUEST["ux_chk_overlayclick"]) ? "true" : "false";
+			$lightbox_setting_array["error_message"] = stripslashes(esc_attr($_REQUEST["ux_cb_errormsg"]));
+			$lightbox_setting_array["language_direction"] = esc_attr($_REQUEST["ux_rdl_enablelanguage"]);
+			$lightbox_setting_array["disable_other_lightbox"] = isset($_REQUEST["ux_chk_disablelightbox"]) ? "true" : "false";
+		
+			foreach ($lightbox_setting_array as $val => $innerKey) 
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"UPDATE " . wp_lightbox_bank_settings() . " SET setting_value = %s WHERE setting_key = %s",
+						$innerKey ,
+						$val 
+					)
+				);
+			}
+			die();
+		}
+		elseif($_REQUEST["param"] == "update_display_settings")
+		{
+			$display_setting_array = array();
+			$display_setting_array["image_title"]= isset($_REQUEST["ux_image_title"]) ? "true" : "false";
+			$display_setting_array["image_caption"] = isset($_REQUEST["ux_chk_image_caption"]) ? "true" : "false";
+			$display_setting_array["text_align"] = esc_attr($_REQUEST["ux_text_align"]);
+			foreach ($display_setting_array as $val => $innerKey)
+			{
+				$wpdb->query
+				(
+					$wpdb->prepare
+					(
+						"UPDATE " . wp_lightbox_bank_settings() . " SET setting_value = %s WHERE setting_key = %s",
+						$innerKey ,
+						$val
+					)
+				);
+			}
+			die();
+		}
+		elseif ($_REQUEST["param"] == "restore_settings")
+		{
+			$sql = "TRUNCATE TABLE " . wp_lightbox_bank_settings();
+			$wpdb->query($sql);
+			
+			include_once (WP_LIGHTBOX_BANK_BK_PLUGIN_DIR . "/lib/include-lightbox-bank-settings.php");
+			die();
+		}
+	}
+}	
+ ?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+﻿=== WP Lightbox Bank ===
+Contributors: contact-bank,Gallery-Bank
+Tags: lightbox, Lightbox slider, popup, lightview, overlay, popup, popup image, popup images, colorbox, lightbox popup, lightbox pop, on click popup, onclick popup, popup box, style, style lightbox, fancybox, wordpress image lightbox, wordpress lightbox, wordpress popup, popup photo, photo, shadowbox, img, image, thickbox, open image, video lightbox, page, post
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WP Lightbox Bank is the perfect responsive image lightbox for WordPress Galleries & Albums.
+
+== Description ==
+
+Lightbox is a perfect responsive image lightbox for viewing photos and videos. 
+
+It is created especially for hassle free usage, permits you to view larger version of images and videos and giving an interesting design. 
+
+Videos from two sources, i.e Vimeo and YouTube can be played in the lightbox bank.
+
+**Features**
+
+*	Hassle Free
+*	Responsive Design
+*	Works with WordPress Galleries
+*	Works with WordPress Captioned Images
+*	Almost Zero Configuration
+
+
+### If you think, that you found a bug in our [WP Lightbox Bank](http://tech-banker.com) plugin or have any question contact us at [support@tech-banker.com](mailto:support@tech-banker.com).
+
+== Installation ==
+
+### First download the ZIP file ,
+
+1. Log in to your website administrator panel.   
+2. Go to Plugins page, and add new plugin.    
+3. Upload [WordPress Lightbox](http://wordpress.org/plugins/lightbox). 
+4. Click `Install Now` button.     
+5. Then click `Activate Plugin` button. 
+
+Now you can set your lightbox options, images and use our lightbox.
+
+== Screenshots ==
+    
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial Release
